c9140802125447e3590375e8c99b14a9d8837c64
dlevenstein <dan.levenstein@gmail.com>
2016-02-24 10:11:05 -0500

    PlayLFPTV Function
    moved display functionality to PlayLFPTV function

--- Plotting/LFPTV/LFPTV.m
+++ Plotting/LFPTV/LFPTV.m
@@ -51,9 +51,10 @@ ICellSpikes = ICellSpikes(Isort_rate);
 
 
 %% Parms
-windowsize = 10; % s
+windowsize = 8; % s
+timedilation = 0.01; %s/s    not accurate... is dependent on CPU etc
+
 framesize = 0.01; %s
-timedilation = 0.01; %not accurate... is dependent on CPU etc
 pausetime = timedilation*framesize; %frames/s
 
 %vidtime = [800 2300];
@@ -114,15 +115,13 @@ SPs = [Start(StateIntervals.Spindles,'s') End(StateIntervals.Spindles,'s')];
     noverlap = noverlap*sf_LFP;
     [FFTspec,FFTfreqs,t_FFT] = spectrogram(allLFP,window,noverlap,freqs,sf_LFP);
     FFTspec = abs(FFTspec);
-    [zFFTspec,mu,sig] = zscore(log10(FFTspec)');
 
 
 
 
 
 %% PlayLFPTV %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-globalmap.spec 
-globalmap.state
+
 
 
 
@@ -130,10 +129,15 @@ spikets
 cellgroups
 
 %% 
+globalmap.spec = FFTspec;
+globalmap.t = t_FFT;
+globalmap.freqs = FFTfreqs;
+
 lfpmap.spec = zspec;
 lfpmap.LFP = LFP;
 lfpmap.freqs = freqs;
 lfpmap.t_LFP = t_LFP;
+lfpmap.sf = sf_LFP;
 
 spkidx.pE = spindices;
 spkidx.pI = Ispindices;
@@ -155,102 +159,11 @@ plotparms.spkgroupcolors = {'k','r'};
 tparms.vidtime = vidtime;
 tparms.framedt = framesize;
 tparms.framewidth = windowsize;
+tparms.pausetime = pausetime;
+
 
 %%
-statenames = fieldnames(stateints);
-numstates = length(statenames);
-
-eventnames = fieldnames(eventints);
-numevents = length(eventnames);
-
-cellgroupnames = fieldnames(spkidx); 
-numcellgroups = length(cellgroupnames);
-
-%%
-%idea!  loop only over xlim....
-%% Figure
-figure
-%for t1 = tparms.vidtime(1):tparms.framedt:tparms.vidtime(2)
-t1 = tparms.vidtime(1);
-
-viewwin = [t1 t1+tparms.framewidth];
-viewwin_LFP = [round(viewwin(1)*sf_LFP):round(viewwin(2)*sf_LFP)]+1;
-viewwin_LFP = lfpmap.t_LFP>=viewwin(1) & lfpmap.t_LFP<=viewwin(2);
-
-%Global Recording Ref Map
-    subplot(6,2,1)
-        imagesc(t_FFT,log2(freqs),log10(FFTspec))
-        axis xy
-        set(gca,'YTick',(log2([1 2 4 8 16 32 64 128])))
-        set(gca,'YTickLabel',{'1','2','4','8','16','32','64','128'})
-        caxis([3.5 6.5])
-        caxis([min(mu)-2.5*max(sig) max(mu)+2.5*max(sig)])
-        %xlim(viewwin)
-        %colorbar('east')
-        ylim([log2(freqs(1)) log2(freqs(end))+0.2])
-        ylabel({'LFP - FFT','f (Hz)'})
-    
-    hold on
-    for ss = 1:numstates
-        stateint = stateints.(statenames{ss});
-        plot(stateint',log2(freqs(end))*ones(size(stateint')),plotparms.statecolors{ss},'LineWidth',8)
-    end
-        plot([t1 t1],get(gca,'ylim'),'w')
-            
-        %ylim([0.99 1.01])
-        %xlim(vidtime)
-        set(gca,'XTickLabel',[]);set(gca,'XTick',[]);
-        set(gca,'YTickLabel',[])
-
-%Wavelet Spectrogram    
-    subplot(6,1,2:4)
-        imagesc(lfpmap.t_LFP(viewwin_LFP),log2(lfpmap.freqs),lfpmap.spec(:,viewwin_LFP))
-        set(gca,'YTick',(log2([1 2 4 8 16 32 64 128])))
-        set(gca,'YTickLabel',{'1','2','4','8','16','32','64','128'})
-        set(gca,'XTickLabel',[])
-        xlim(viewwin)
-        caxis([-1 4])
-        axis xy
-
-%LFP and Spikes     
-    subplot(6,1,5:6)    
-        hold on
-    for ss = 1:numstates
-        stateint = stateints.(statenames{ss});
-        lineheight = 4;
-        plot(stateint',lineheight*ones(size(stateint')),...
-            plotparms.statecolors{ss},'LineWidth',3)
-    end
-    for ee = 1:numevents
-        lineheight = 3.5-0.1*ee;
-        eventint = eventints.(eventnames{ee});
-        plot(eventint',lineheight*ones(size(eventint')),...
-            plotparms.eventcolors{ee},'LineWidth',1)
-    end
-        plot(lfpmap.t_LFP(viewwin_LFP),lfpmap.LFP(viewwin_LFP),'k')
-
-        xlim(viewwin)
-    
-    hold on
-    groupoffset = -40;
-    cellnumscale = 10;
-    for cc = 1:numcellgroups
-        groupspks = spkidx.(cellgroupnames{cc});
-        plot(groupspks(:,1),(groupoffset-groupspks(:,2))./cellnumscale,...
-            '.','Color',plotparms.spkgroupcolors{cc},'MarkerSize',8)
-        groupoffset = groupoffset-max(groupspks(:,2));
-    end
-        
-        xlim(viewwin);ylim([(groupoffset-1)/cellnumscale 4]);
-        xlabel('t (s)');
-        ylabel('Neuron, Sorted by Rate')
-        
-        
-        pause(pausetime)
-%end
-
-
-
+PlayLFPTV(globalmap,lfpmap,spkidx,stateints,eventints,plotparms,tparms);
 
 end
 
--- Plotting/LFPTV/PlayLFPTV.m
+++ Plotting/LFPTV/PlayLFPTV.m
@@ -1,64 +1,103 @@
-function [ output_args ] = PlayLFPTV( input_args )
+function [  ] = PlayLFPTV(globalmap,lfpmap,...
+    spkidx,stateints,eventints,plotparms,tparms)
 %PlayLFPTV (inputs) 
 %% DEV
 
 
 %%
+[~,mu,sig] = zscore(log10(globalmap.spec)');
+
+statenames = fieldnames(stateints);
+numstates = length(statenames);
+
+eventnames = fieldnames(eventints);
+numevents = length(eventnames);
+
+cellgroupnames = fieldnames(spkidx); 
+numcellgroups = length(cellgroupnames);
+
+%%
+%idea!  loop only over xlim....
+%need to downsample LFP 
 %% Figure
 figure
-for t1 = vidtime(1):framesize:vidtime(2)
+for t1 = tparms.vidtime(1):tparms.framedt:tparms.vidtime(2)
+%t1 = tparms.vidtime(1);
 
+viewwin = [t1 t1+tparms.framewidth];
+viewwin_LFP = [round(viewwin(1)*lfpmap.sf):round(viewwin(2)*lfpmap.sf)]+1;
+viewwin_LFP = lfpmap.t_LFP>=viewwin(1) & lfpmap.t_LFP<=viewwin(2);
 
-%t1 = 0;
-viewwin = [t1 t1+windowsize];
-viewwin_LFP = [round(viewwin(1)*sf_LFP):round(viewwin(2)*sf_LFP)]+1;
-viewwin_LFP = t_LFP>=viewwin(1) & t_LFP<=viewwin(2);
-
-    subplot(18,1,1)
-
-        plot(REM',ones(size(REM')),'g',...
-            SWS',ones(size(SWS')),'b',...
-            Wake',ones(size(Wake')),'r',...
-            t1,1,'w.',...
-            'LineWidth',10)
-        ylim([0.99 1.01])
-        xlim(vidtime)
+%Global Recording Ref Map
+    subplot(12,2,[1,3])
+        imagesc(globalmap.t,log2(globalmap.freqs),log10(globalmap.spec))
+        axis xy
+        set(gca,'YTick',(log2([1 2 4 8 16 32 64 128])))
+        set(gca,'YTickLabel',{'1','2','4','8','16','32','64','128'})
+        caxis([3.5 6.5])
+        caxis([min(mu)-3.5*max(sig) max(mu)+3.5*max(sig)])
+        %xlim(viewwin)
+        %colorbar('east')
+        ylim([log2(globalmap.freqs(1)) log2(globalmap.freqs(end))+0.2])
+        %ylabel({'LFP - FFT','f (Hz)'})
+    
+    hold on
+    for ss = 1:numstates
+        stateint = stateints.(statenames{ss});
+        plot(stateint',log2(globalmap.freqs(end))*ones(size(stateint')),plotparms.statecolors{ss},'LineWidth',8)
+    end
+        plot([t1 t1],get(gca,'ylim'),'w')
+            
+        %ylim([0.99 1.01])
+        %xlim(vidtime)
         set(gca,'XTickLabel',[]);set(gca,'XTick',[]);
         set(gca,'YTickLabel',[])
 
-    subplot(16,1,10:12)
-        plot(REM', 2.4*ones(size(REM')),'g',...
-            SWS', 2.4*ones(size(SWS')),'b',...
-            Wake', 2.4*ones(size(Wake')),'r',...
-            UPs(:,1),2.3*ones(size(UPs(:,1))),'+g',...
-            UPs(:,2),2.3*ones(size(UPs(:,2))),'+r',...
-            SPs(:,1),2*ones(size(SPs(:,1))),'+c',...
-            SPs(:,2),2*ones(size(SPs(:,2))),'+m',...
-            t_LFP(viewwin_LFP),LFP(viewwin_LFP),'k')
-
-        xlim(viewwin)
-        ylim([-3 3])
-
-    
-    subplot(16,1,2:9)
-        imagesc(t_LFP(viewwin_LFP),log2(freqs),zspec(:,viewwin_LFP))
+%Wavelet Spectrogram    
+    subplot(6,1,2:4)
+        imagesc(lfpmap.t_LFP(viewwin_LFP),log2(lfpmap.freqs),lfpmap.spec(:,viewwin_LFP))
         set(gca,'YTick',(log2([1 2 4 8 16 32 64 128])))
         set(gca,'YTickLabel',{'1','2','4','8','16','32','64','128'})
         set(gca,'XTickLabel',[])
         xlim(viewwin)
         caxis([-1 4])
         axis xy
+        ylabel({'LFP - FFT','f (Hz)'})
 
+%LFP and Spikes     
+    subplot(6,1,5:6)    
+        hold on
+    for ss = 1:numstates
+        stateint = stateints.(statenames{ss});
+        lineheight = 4;
+        plot(stateint',lineheight*ones(size(stateint')),...
+            plotparms.statecolors{ss},'LineWidth',3)
+    end
+    for ee = 1:numevents
+        lineheight = 3.5-0.1*ee;
+        eventint = eventints.(eventnames{ee});
+        plot(eventint',lineheight*ones(size(eventint')),...
+            plotparms.eventcolors{ee},'LineWidth',1)
+    end
+        plot(lfpmap.t_LFP(viewwin_LFP),lfpmap.LFP(viewwin_LFP),'k')
+
+        xlim(viewwin)
+    
+    hold on
+    groupoffset = -35;
+    cellnumscale = 10;
+    for cc = 1:numcellgroups
+        groupspks = spkidx.(cellgroupnames{cc});
+        plot(groupspks(:,1),(groupoffset-groupspks(:,2))./cellnumscale,...
+            '.','Color',plotparms.spkgroupcolors{cc},'MarkerSize',8)
+        groupoffset = groupoffset-max(groupspks(:,2));
+    end
         
-    subplot(4,1,4)
-        plot(spindices(:,1),spindices(:,2),'.',...
-            Ispindices(:,1),-Ispindices(:,2),'r.','MarkerSize',6)
-        xlim(viewwin);ylim([-numIcells numcells+1]);
+        xlim(viewwin);ylim([(groupoffset-1)/cellnumscale 4]);
         xlabel('t (s)');
         ylabel('Neuron, Sorted by Rate')
-        
-        
-        pause(pausetime)
+            
+        pause(tparms.pausetime)
 end
 
 end
