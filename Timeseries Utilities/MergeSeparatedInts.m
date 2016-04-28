0ae3c400f4e1bed6327d01889ab34fa9d9d6f9d9
dlevenstein <dan.levenstein@gmail.com>
2016-04-28 08:20:46 -0400

    Update MergeSeparateInts to account for overlapping intervals

--- Timeseries Utilities/MergeSeparatedInts.m
+++ Timeseries Utilities/MergeSeparatedInts.m
@@ -1,15 +1,25 @@
 function [ ints ] = MergeSeparatedInts( ints,minseparation )
-%UNTITLED3 Summary of this function goes here
+%[ ints ] = MergeSeparatedInts( ints,minseparation ) merges close or
+%overlapping intervals with a minimum separation.
 %
 %INPUT
 %   ints            (n_ints x 2) matrix of interval start and end times
 %   minseparation   merge ints separated by less than or equal to min
 %%
+
+%Sort Ints by their end times so all intervals end before the
+%subsequent interval ends
+[~,sort_ints] = sort(ints(:,2));
+ints = ints(sort_ints,:);
+
+%Find ints that start before a minimum separation after the end of the 
+%previous interval
 intseparation = ints(2:end,1)-ints(1:end-1,2);
 smallsep = find(intseparation<=minseparation);
 
-for s = flipud(smallsep)'
+for s = flipud(smallsep)'   %Go backwards from last ints to first ones
     ints(s,2) = ints(s+1,2);
+    ints(s,1) = min(ints(s+1,1),ints(s,1)); %Take the earlier start time between the ints
     ints(s+1,:) = [];
 end
 
