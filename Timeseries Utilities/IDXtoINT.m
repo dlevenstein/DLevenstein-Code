cc401c74dbf0161043203f9fd5e9d3c1bd54767f
DLevenstein <dan.levenstein@gmail.com>
2016-05-24 16:13:08 -0400

    IDXtoINT now has optional input for number of states total - robust to missing states in the IDX vector

--- Timeseries Utilities/IDXtoINT.m
+++ Timeseries Utilities/IDXtoINT.m
@@ -1,17 +1,23 @@
-function [ INT ] = IDXtoINT( IDX )
+function [ INT ] = IDXtoINT( IDX ,numstates)
 %IDXtoINT(IDX) Converts state indices to state on/offsets
 %
 %INPUT
 %   IDX:    [t x 1] vector of state indices, where states are identified by
 %           integers starting from 1.
+%   numstates (optional)  number of interval types (for use
 %
 %OUTPUT
 %   INT:    {nstates} cell array of intervals - start and end times
+%
+%DLevenstein 2015-16
+%%
 
-
-
-states = unique(IDX);
-numstates = length(states);
+if exist('numstates','var')
+    states = 1:numstates;
+else
+    states = unique(IDX);
+    numstates = length(states);
+end
 
 IDX = [0; IDX; 0];
 for ss = 1:numstates
