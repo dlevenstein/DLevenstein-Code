04afb39493b607ba096d6c37b7c45b67727410a9
DLevenstein <dan.levenstein@gmail.com>
2017-04-20 19:47:42 -0400

    bz_LoadStates has basePath style input

--- Database Management/bz_LoadStates.m
+++ Database Management/bz_LoadStates.m
@@ -1,13 +1,17 @@
-function [ states ] = bz_LoadStates( statesName,baseName,datasetPath )
-%[ states ] = bz_LoadStates( statesName,baseName,datasetPath ) function for
+function [ states ] = bz_LoadStates( basePath,statesName )
+%[ states ] = bz_LoadStates(basePath, statesName) function for
 %loading states.mat files. states.mat files are saved as...
 % datasetPath/baseName/baseName.statesName.states.mat
 %
 %statesName can be the name of a states.mat file, or can be 'all' to load
-%all states.mat files for a given recording.
+%all states.mat files for a given recording. If empty, prompts the user
+%with a list of available states.mat files in basePath
 %
 %DLevenstein 2017
 %%
+
+[datasetPath,baseName] = fileparts(basePath);
+
 % if strcmp('statesName','all')
 %     allStates = dir(fullfile(datasetPath,baseName,[baseName,'.','*','.states.mat']));
 %     for
