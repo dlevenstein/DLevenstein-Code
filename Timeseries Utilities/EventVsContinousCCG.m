2037d7f47217cf45fe58f1c0e8339b0c48fdfde7
DLevenstein <dan.levenstein@gmail.com>
2017-04-19 21:54:19 -0400

    added continuous CG function

--- Timeseries Utilities/EventVsContinousCCG.m
+++ Timeseries Utilities/EventVsContinousCCG.m
@@ -1,4 +1,4 @@
-function [CCGmean,t_lag,CCGstd,CCGevents ] = EventVsContinousCCG( data,data_t,events,lagwin )
+function [CCGmean,t_lag,CCGstd,CCGevents,skippedevents ] = EventVsContinousCCG( data,data_t,events,lagwin )
 %Data should be continuous time series associated with time stamps data_t
 %events should be in seconds, as should lagwin
 %%
@@ -13,13 +13,15 @@ t_lag_dt = round(lagwin./data_dt);
 t_lag = [-t_lag_dt:t_lag_dt].*data_dt;
 
 %Get the time points closest to events, and samples in window around them
-events_dt = find(ismember(data_t,event_ints));
+[events_dt] = find(ismember(data_t,event_ints));
 events_dt = bsxfun(@(A,B) A+B,events_dt,t_lag_dt.*[-1 1]);
 
-[CCGevents,~] = IsolateEpochs2(data,events_dt,0,1);
+skippedevents = (isnan(event_ints));
+
+[CCGevents,~] = IsolateEpochs2(data,events_dt,0,1,'includeNaN');
 CCGevents = cat(2,CCGevents{:});
-CCGmean = mean(CCGevents,2);
-CCGstd = std(CCGevents,[],2);
+CCGmean = nanmean(CCGevents,2);
+CCGstd = nanstd(CCGevents,[],2);
 
 end
 
