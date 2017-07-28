890ddedbb0bbb8e87319d303a2bcb73ddaa9a895
dlevenstein <dan.levenstein@gmail.com>
2017-07-28 09:29:21 -0400

    Fixed bug for three-int pairs in MergeSeparatedInts

--- Timeseries Utilities/MergeSeparatedInts.m
+++ Timeseries Utilities/MergeSeparatedInts.m
@@ -11,26 +11,35 @@ function [ newints,mergedidx ] = MergeSeparatedInts( ints,minseparation )
 %                   -sorted
 %   mergedidx       {n_newints} indices of which old intervals were merged
 %                   into each new interval
+%
+%DLevenstein 2015
+%%
+
 %% Test
 % ints = [0 3;2 4; 6 10; 10.5 13;  20 30; 19 21];
 % minseparation = 0.5;
 
+ints = [0 10; 11 19; 9 20; 25 27; 26 28];   %BUG!
 
 %%
 %Sort Ints by their end times so all intervals end before the
 %next interval ends
 [~,sort_ints] = sort(ints(:,2));
 newints = ints(sort_ints,:);
-mergedidx = num2cell(sort_ints);
+mergedidx = num2cell(sort_ints); %Keep track of which ints correspond to which old ints
 
 if ~exist('minseparation','var')
     minseparation = 0;
 end
 
-%Find ints that start before a minimum separation after the end of the 
+%Find ints that start before a minimum separation after the end of ANY 
 %previous interval
-intseparation = newints(2:end,1)-newints(1:end-1,2);
-smallsep = find(intseparation<=minseparation);
+%This will be a matrix in which element ij is the difference between the
+%start of interval i and the end of interval j
+allintseparation = bsxfun(@minus, newints(:,1)',newints(:,2));
+smallsep = allintseparation<=minseparation; %S-E pairs all intervals
+smallsep = triu(smallsep,1); %take only the interval pars where i ENDS after j
+smallsep = find(any(smallsep,2)); %intervals who start before ANY previous interval
 
 for s = flipud(smallsep)'   %Go backwards from last ints to first ones
     newints(s,2) = newints(s+1,2);  %Take the later end time between the ints
@@ -39,6 +48,14 @@ for s = flipud(smallsep)'   %Go backwards from last ints to first ones
     mergedidx{s} = [mergedidx{s} mergedidx{s+1}];
     mergedidx(s+1) = [];
 end
+
+
+%Test for further merges needed - bug fix for case with two non-overlapping
+%intervals subsumed in a larger interval. Like follows:
+% ------
+%          --------
+%     ---------------
+
 
 end
 
