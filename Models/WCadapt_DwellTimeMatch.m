c43fe7232a3ab5415fd8a79ff29e3411733a35b4
DLevenstein <dan.levenstein@gmail.com>
2018-05-09 13:47:38 -0400

    dwelltimematch changes'

--- Models/WCadapt_DwellTimeMatch.m
+++ Models/WCadapt_DwellTimeMatch.m
@@ -1,4 +1,4 @@
-function [dwelltimes_sim_sf,dwellfit] = WCadapt_DwellTimeMatch(dwelltimestomatch,simdwelltimes,scalefactor)
+function [dwelltimes_sim_sf,dwellfit] = WCadapt_DwellTimeMatch(dwelltimestomatch,simdwelltimes,varargin)
 %[matcheddwelltimes] = WCadapt_DwellTimeMatch(dwelltimestomatch,simdwelltimes)
 %
 %INPUT
@@ -9,36 +9,51 @@ function [dwelltimes_sim_sf,dwellfit] = WCadapt_DwellTimeMatch(dwelltimestomatch
 %   scalefactor (optional) single value or range
 %   
 %%
+p = inputParser;
+addParameter(p,'scalefactor',[0.001 0.025])
+addParameter(p,'showprogress',true,@islogical)
+
+parse(p,varargin{:})
+scalefactor = p.Results.scalefactor;
+showprogress = p.Results.showprogress;
+
+%%
 if isempty(dwelltimestomatch.UP) || isempty(dwelltimestomatch.DOWN)
     dwelltimes_sim_sf = [];dwellfit=[];
     return
 end
 
-if ~exist('scalefactor','var')
-    scalefactor = [0.001 0.025];
-end
+
 
 numsims = length(simdwelltimes);
-parfor_progress(numsims);
-tstart = tic;
+if showprogress
+        parfor_progress(numsims);
+end
+    tstart = tic;
+
+
 parfor nn = 1:numsims
 
-    timespent=toc(tstart);
-    percdone = parfor_progress;
+    if showprogress
+        timespent=toc(tstart);
+        percdone = parfor_progress;
 
-    estimatedtotal = timespent./(percdone./100);
-    estimatedremaining = estimatedtotal-timespent;
-    display(['Percent Done: ',num2str(percdone),...
-        '.  Time Spent: ',num2str(round(timespent./60,1)),...
-        '.  Est. Total Time: ',num2str(round(estimatedtotal./60,1)),...
-        'min.  ETR: ',num2str(round(estimatedremaining./60,1)),'min.'])
+        estimatedtotal = timespent./(percdone./100);
+        estimatedremaining = estimatedtotal-timespent;
+        display(['Percent Done: ',num2str(percdone),...
+            '.  Time Spent: ',num2str(round(timespent./60,1)),...
+            '.  Est. Total Time: ',num2str(round(estimatedtotal./60,1)),...
+            'min.  ETR: ',num2str(round(estimatedremaining./60,1)),'min.'])
+    end
     
         [dwellfit(nn).bestsf,dwelltimes_sim_sf(nn),...
             dwellfit(nn).KSSTAT,dwellfit(nn).selectionparm]...
             = FindBestScaleFactor(dwelltimestomatch,simdwelltimes(nn),scalefactor);
 end
 
-parfor_progress(0);
+if showprogress
+    parfor_progress(0);
+end
 
 end
 
